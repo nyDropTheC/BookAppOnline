35dada5aab5f45283d67c8f216f6c8cfa570df3f
Nik <125159296+nyDropTheC@users.noreply.github.com>
2023-11-02 22:39:02 +0200

    Initial commit

---
+++ src/main/java/BackendProg/BookAppWeb/BookAppWebApplication.java
@@ -0,0 +1,13 @@
+package BackendProg.BookAppWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppWebApplication.class, args);
+	}
+
+}
