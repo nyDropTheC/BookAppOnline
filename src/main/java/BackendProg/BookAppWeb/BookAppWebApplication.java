c9ef6e28553ef9ffad494f03d46276123d1457d4
nyDropTheC <125159296+nyDropTheC@users.noreply.github.com>
2023-11-20 15:59:07 +0200

    AAAAAAA

--- src/main/java/BackendProg/BookAppWeb/BookAppWebApplication.java
+++ src/main/java/BackendProg/BookAppWeb/BookAppWebApplication.java
@@ -1,7 +1,11 @@
 package BackendProg.BookAppWeb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.theokanning.openai.service.OpenAiService;
 
 @SpringBootApplication
 public class BookAppWebApplication {
@@ -9,5 +13,4 @@ public class BookAppWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookAppWebApplication.class, args);
 	}
-
 }
---
+++ src/main/java/BackendProg/BookAppWeb/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package BackendProg.BookAppWeb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        return http.build();
+    }
+}
