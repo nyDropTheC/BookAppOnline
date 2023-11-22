aacef70fa26122003336f05b47117b1dbaaa7ab5
nyDropTheC <125159296+nyDropTheC@users.noreply.github.com>
2023-11-22 16:31:13 +0200

    Initial OpenAI testing

--- src/main/java/BackendProg/BookAppWeb/BookAppWebApplication.java
+++ src/main/java/BackendProg/BookAppWeb/BookAppWebApplication.java
@@ -1,12 +1,27 @@
 package BackendProg.BookAppWeb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import BackendProg.BookAppWeb.util.OpenAiComms;
 
 @SpringBootApplication
 public class BookAppWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookAppWebApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+			System.out.println("Hello from bookapponline!");
+			
+			OpenAiComms comms = new OpenAiComms();
+
+			System.out.println(comms.getBookRecommendationText(null, null));
+		};
 	}
 }
--- src/main/java/BackendProg/BookAppWeb/util/OpenAiComms.java
+++ src/main/java/BackendProg/BookAppWeb/util/OpenAiComms.java
@@ -1,11 +1,19 @@
 package BackendProg.BookAppWeb.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest.ChatCompletionRequestBuilder;
 import com.theokanning.openai.service.OpenAiService;
 
 public class OpenAiComms {
     private OpenAiService remoteAiService;
 
     public OpenAiComms() {
+        System.out.println("Openai key: " + System.getenv("BOOKAPP_OPENAI_KEY"));
         remoteAiService = new OpenAiService(System.getenv("BOOKAPP_OPENAI_KEY"));
     }
 
@@ -14,6 +22,22 @@ public class OpenAiComms {
     }
 
     public String getBookRecommendationText(String bookName, String bookAuthors) {
-        return "";
+        List<ChatMessage> messages = new ArrayList<>();
+
+        messages.add(new ChatMessage(
+            ChatMessageRole.USER.value(), 
+            "Hello there! Please introduce yourself in detail."
+        ));
+
+        ChatCompletionRequest req = ChatCompletionRequest
+                                        .builder()
+                                        .model("gpt-3.5-turbo")
+                                        .messages(messages)
+                                        .temperature(1.1)
+                                        .build();
+
+        ChatMessage resp = getService().createChatCompletion(req).getChoices().get(0).getMessage();
+        
+        return resp.getContent();
     }
 }
