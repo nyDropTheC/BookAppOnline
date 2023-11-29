d735861f067e78d9d1a4e5a7977015c478016411
Nik <125159296+nyDropTheC@users.noreply.github.com>
2023-11-29 23:30:24 +0200

    Should be ready for deployment now

--- src/main/java/BackendProg/BookAppWeb/controller/rest/OpenAiBookRequestController.java
+++ src/main/java/BackendProg/BookAppWeb/controller/rest/OpenAiBookRequestController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import BackendProg.BookAppWeb.model.Book;
 import BackendProg.BookAppWeb.repository.BookRepository;
+import BackendProg.BookAppWeb.util.DiscordIdHelper;
 import BackendProg.BookAppWeb.util.OpenAiComms;
 
 @RestController
@@ -24,17 +26,19 @@ public class OpenAiBookRequestController {
     
     // Not a GET, does not follow rules of GET (e.g. won't cache right, affects stuff in DB and shit)
     // But Spring Security breaks when it's a POST request for some reason
-    @GetMapping("/api/openai/summarize/{bookId}")
-    public String summarizeBook(@RequestHeader(HttpHeaders.AUTHORIZATION) String userId, @PathVariable("bookId") Long bookId) {
-        Book book = bookRepo.findById(bookId).orElse(null);
+    // NVM maybe it won't break if we call it from the actual website instead of Postman?
 
+    @GetMapping("/api/openai/summarize/{bookId}")
+    public String summarizeBook(@PathVariable("bookId") Long bookId) {
+        Book book = bookRepo.findById(bookId).orElse(null);
+        String discordUser = DiscordIdHelper.getCurrentDiscordUser(); // Curious lol
         // this is horrifying and retarded
         if(book == null) {
             return "";
         }
 
         // and not secure at all, given an user can just bullshit their Discord ID
-        if(!book.getDiscordUser().equals(userId)) {
+        if(!book.getDiscordUser().equals(discordUser)) {
             return "";
         }
 
